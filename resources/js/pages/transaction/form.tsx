f7e39e7187d701b25a3b984ba7ce1bc889166d2b
Kayla <satitiputri.kayla@gmail.com>
2025-10-27 15:59:22 +0700

    conflict

--- resources/js/pages/transaction/form.tsx
+++ resources/js/pages/transaction/form.tsx
@@ -7,11 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Label } from '@/components/ui/label';
 import { PlusCircle } from 'lucide-react';
-<<<<<<< HEAD
-import { type Transaction } from './Index'; // Mengimpor interface dari file Index
-=======
 import { type Transaction } from './index'; // Mengimpor interface dari file Index
->>>>>>> 2fc60e6 (fix form description)
 
 // --- INTERFACES ---
 interface Props {
@@ -36,11 +32,7 @@ type FormData = {
     tags: number[]; // Diubah untuk menampung banyak ID tag
     amount: string;
     type: 'income' | 'outcome' | 'transfer' | string;
-<<<<<<< HEAD
-    description: string;
-=======
     description: string | null;
->>>>>>> 2fc60e6 (fix form description)
     transaction_date: string;
 };
 
@@ -65,11 +57,7 @@ export default function TransactionFormModal({
         tags: [], // Nilai awal adalah array kosong
         amount: '',
         type: defaultType,
-<<<<<<< HEAD
-        description: '',
-=======
         description: null,
->>>>>>> 2fc60e6 (fix form description)
         transaction_date: new Date().toISOString().slice(0, 10),
     });
 
@@ -133,11 +121,7 @@ export default function TransactionFormModal({
                         {data.id ? 'Edit' : 'Add'} {data.type.charAt(0).toUpperCase() + data.type.slice(1)}
                     </DialogTitle>
                 </DialogHeader>
-<<<<<<< HEAD
-                <form onSubmit={handleSubmit} className="mt-2 space-y-4">
-=======
                 <form onSubmit={handleSubmit} noValidate className="mt-2 space-y-4">
->>>>>>> 2fc60e6 (fix form description)
                     <InputField
                         label="Title"
                         type="text"
@@ -186,11 +170,7 @@ export default function TransactionFormModal({
                         id="description"
                         type="text"
                         value={data.description}
-<<<<<<< HEAD
-                        onChange={(e) => setData('description', e.target.value)}
-=======
                         onChange={(e) => setData('description', e.target.value || null)}
->>>>>>> 2fc60e6 (fix form description)
                     />
 
                     <div className="flex items-end gap-2">
